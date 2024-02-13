9a19702375fc4bcc840636c060bba2acb7495d05
naoyuki <kamaboko912@gmail.com>
2024-02-13 19:28:18 +0900

    編集時のバリデーション表示

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -85,7 +85,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                             <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
                         </div>
                         <div
-                            id="customer-error"
+                            id="amount-error"
                             aria-live="polite"
                             aria-atomic="true"
                         >
@@ -157,7 +157,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                             ))}
                     </div>
                     <div
-                        id="customer-error"
+                        id="message-error"
                         aria-live="polite"
                         aria-atomic="true"
                     >
